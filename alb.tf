ee90cc68d580a855e91be988885e79c78a361cbd
EC2 Default User <ec2-user@ip-10-0-12-99.ap-northeast-1.compute.internal>
2024-08-16 09:04:45 +0000

    Create Application Load Balancer

--- alb.tf
+++ alb.tf
@@ -2,10 +2,10 @@ resource "aws_lb" "alb" {
   name               = "${local.app}-alb"
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb.id]
-  subnets            = [module.vpc.public_subnets]
+  subnets            = module.vpc.public_subnets
 }
 
-resource "aws_lb_listener" "alb" {
+resource "aws_lb_listener" "alb_listener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = "80"
   protocol          = "HTTP"
@@ -22,7 +22,7 @@ resource "aws_lb_listener" "alb" {
 }
 
 resource "aws_lb_listener_rule" "alb_listener_rule" {
-  listener_arn = aws_lb_listener.alb.arn
+  listener_arn = aws_lb_listener.alb_listener.arn
 
   action {
     type             = "forward"
@@ -37,17 +37,17 @@ resource "aws_lb_listener_rule" "alb_listener_rule" {
 }
 
 resource "aws_lb_target_group" "target_group" {
-  name     = "${local.app}-target-group"
-  port     = 8080
-  protocol = "HTTP"
+  name        = "${local.app}-target-group"
+  port        = 8080
+  protocol    = "HTTP"
   target_type = "ip"
-  vpc_id   = module.vpc.vpc_id
+  vpc_id      = module.vpc.vpc_id
 
   health_check {
-    healthy_threshold = 2
-    timeout = 5
-    interval = 30
-    path = "/health_checks"
-    protocol = "HTTP"
+    healthy_threshold = 3
+    interval          = 30
+    path              = "/health_checks"
+    protocol          = "HTTP"
+    timeout           = 5
   }
 }
