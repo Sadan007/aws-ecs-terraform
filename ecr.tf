23b5663cde6ae094f7a8438fb80e40b48dff6cf8
EC2 Default User <ec2-user@ip-10-0-12-99.ap-northeast-1.compute.internal>
2024-08-16 09:52:25 +0000

    Create ECR repository

--- ecr.tf
+++ ecr.tf
@@ -1,7 +1,7 @@
 resource "aws_ecr_repository" "ecr_repository" {
   name                 = local.app
   image_tag_mutability = "IMMUTABLE"
-  force_delete = true
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -28,6 +28,5 @@ resource "aws_ecr_lifecycle_policy" "ecr_lifecycle_policy" {
     }
   ]
 }
-
 EOF
 }
